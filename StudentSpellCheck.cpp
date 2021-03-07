d2d984f547743b723e43f56ef5641676b37d9de5
Kyle Chui <48545987+agentfire170@users.noreply.github.com>
2021-03-07 10:52:44 -0800

    Spell check works (sometimes)
    it works for short words such as "ae", but not "donet"

--- StudentSpellCheck.cpp
+++ StudentSpellCheck.cpp
@@ -26,35 +26,50 @@ bool StudentSpellCheck::load(std::string dictionaryFile) {
 }
 
 bool StudentSpellCheck::spellCheck(std::string word, int max_suggestions, std::vector<std::string>& suggestions) {
+	// See if the word is valid or not
 	bool foundWord = findString(word);
+	// Initialise the current number of suggestions to zero and clear all previous suggestions
 	int count = 0;
 	suggestions.clear();
+	// Initialise the string that will find "close" matches
+	string similar = "";
+	// If the word is not in the dictionary
 	if (!foundWord)
 	{
+		// Go through each position of the word
 		for (int i = 0; i < word.size(); i++)
 		{
+			// Replace the ith character with all 26 letters and an apostrophe, and see if those are
+			// valid words in the dictionary
 			for (int j = 0; j < 26; j++)
 			{
-				string temp(1, 'a' + i);
-				string similar = word.substr(0, i) + temp + word.substr(i + 1);
+				// Create a modified word by changing one character
+				string temp(1, 'a' + j);
+				similar = word.substr(0, i) + temp + word.substr(i + 1);
+				// If the modified word is in the dictionary, push that word into the suggestions
 				if (findString(similar))
 				{
 					count++;
 					suggestions.push_back(similar);
 				}
-				if (count > max_suggestions)
+				// If the count reaches the max, return
+				if (count >= max_suggestions)
 					return false;
 			}
-			string similar = word.substr(0, i) + "\'" + word.substr(i + 1);
-			if (findString(similar))
+			// Modify the word by replacing the ith character with an apostrophe
+			similar = word.substr(0, i) + "\'" + word.substr(i + 1);
+			// If the modified word is in the dictionary, push that word into the suggestions
+			if (word[i] != '\'' && findString(similar))
 			{
 				count++;
 				suggestions.push_back(similar);
 			}
+			// If the count reaches the max, return
 			if (count > max_suggestions)
 				return false;
 		}
 	}
+	// If the word is in the dictionary, return true
 	else
 		return true;
 }
@@ -65,7 +80,7 @@ void StudentSpellCheck::spellCheckLine(const std::string& line, std::vector<Spel
 	string temp = "";
 	for (int i = 0; i < line.size(); i++)
 	{
-		char c =line[i];
+		char c = line[i];
 		if (isalpha(c))
 		{
 			c = tolower(c);
