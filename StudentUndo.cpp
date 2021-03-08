8a7c201dbe995d73dd4529f33b36a7861528d6c4
Kyle Chui <48545987+agentfire170@users.noreply.github.com>
2021-03-08 12:31:03 -0800

    finished comments for undo get

--- StudentUndo.cpp
+++ StudentUndo.cpp
@@ -41,8 +41,10 @@ StudentUndo::Action StudentUndo::get(int &row, int &col, int& count, std::string
 		col += count - 1;
 		return Undo::Action::DELETE;
 	}
+	// If the most recent action is a deletion
 	else if (act == Action::DELETE)
 	{
+		// Check for more deletions right after the current one
 		text = ch;
 		count++;
 		while (!m_stack.empty() &&
@@ -50,6 +52,7 @@ StudentUndo::Action StudentUndo::get(int &row, int &col, int& count, std::string
 			m_stack.top()->m_row == row &&
 			m_stack.top()->m_col - 1 == col)
 		{
+			// Increment count and add to text, and move back one
 			count++;
 			col = m_stack.top()->m_col;
 			ch = m_stack.top()->m_char;
@@ -61,16 +64,21 @@ StudentUndo::Action StudentUndo::get(int &row, int &col, int& count, std::string
 			ch = m_stack.top()->m_col;
 			m_stack.pop();
 		}
+		// Move the column back by count - 1 positions, to the correct spot to insert text
 		col -= count - 1;
 		return Undo::Action::INSERT;
 	}
+	// If the most recent action joins two lines
 	else if (act == Action::JOIN)
 	{
+		// Return that editor should split lines
 		count = 1;
 		return Undo::Action::SPLIT;
 	}
+	// If the most recent action splits two lines
 	else if (act == Action::SPLIT)
 	{
+		// Return that editor should join lines
 		count = 1;
 		return Undo::Action::JOIN;
 	}
